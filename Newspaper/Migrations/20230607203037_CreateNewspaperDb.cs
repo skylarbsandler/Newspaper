b3b33ab197c428560f07f7505ca90ec0c54a810c
skylarbsandler <skylarbsandler@gmail.com>
2023-06-07 14:31:14 -0600

    Created Newspaper Context

---
+++ Newspaper/Migrations/20230607203037_CreateNewspaperDb.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Newspaper.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateNewspaperDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "issues",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    issue_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_issues", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "reporters",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    speciality = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_reporters", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "articles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    content = table.Column<string>(type: "text", nullable: false),
+                    reporter_id = table.Column<int>(type: "integer", nullable: false),
+                    issue_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_articles", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_articles_issues_issue_id",
+                        column: x => x.issue_id,
+                        principalTable: "issues",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_articles_reporters_reporter_id",
+                        column: x => x.reporter_id,
+                        principalTable: "reporters",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_articles_issue_id",
+                table: "articles",
+                column: "issue_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_articles_reporter_id",
+                table: "articles",
+                column: "reporter_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "articles");
+
+            migrationBuilder.DropTable(
+                name: "issues");
+
+            migrationBuilder.DropTable(
+                name: "reporters");
+        }
+    }
+}
---
+++ Newspaper/NewspaperContext.cs
@@ -0,0 +1,23 @@
+﻿using Newspaper.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newspaper
+{
+    public class NewspaperContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Reporter> Reporters { get; set; }
+        public DbSet<Issue> Issues { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Username=postgres;Password=password123;Database=Newspaper")
+                .UseSnakeCaseNamingConvention();
+        }
+    }
+}
