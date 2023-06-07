0ce8140d47420cd3e0828c7d3d47e36ee1b619be
skylarbsandler <skylarbsandler@gmail.com>
2023-06-07 13:33:58 -0600

    Add Issue Class

---
+++ Newspaper/Issue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newspaper
+{
+    public class Issue
+    {
+        public DateTime IssueDate { get; set; }
+        public List<Article> Articles { get; set; } = new List<Article>();
+    }
+}
