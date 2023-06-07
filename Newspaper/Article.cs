e2ecfb2d6064037419809bdfd465a7938927fa1b
skylarbsandler <skylarbsandler@gmail.com>
2023-06-07 13:36:04 -0600

    Add Article Class

---
+++ Newspaper/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newspaper
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Reporter { get; set; }
+        public Issue Issue { get; set; }
+    }
+}
